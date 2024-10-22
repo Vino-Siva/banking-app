d646c1a683d9492706375a72a7e30a6b09e500ed
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-10-22 16:43:23 +0530

    change bankcard to display username in uppercase

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -14,7 +14,7 @@ const BankCard = ({
         <div className="bank-card_content">
           <div className="">
             <h1 className="text-16 font-semibold text-white">
-              {account.name || userName}
+              {account?.name?.toUpperCase() || userName.toUpperCase()}
             </h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
@@ -22,7 +22,9 @@ const BankCard = ({
           </div>
           <article className="flex flex-col gap-2">
             <div className="flex justify-between">
-              <h1 className="text-12 font-semibold text-white">{userName}</h1>
+              <h1 className="text-12 font-semibold text-white">
+                {userName.toUpperCase()}
+              </h1>
               <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
