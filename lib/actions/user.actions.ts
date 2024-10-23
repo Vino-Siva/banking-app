889ea03c0cb880b01e314f92695a0d117c159a07
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-10-23 07:41:37 +0530

    Add and refactor user.actions.ts to handle sign-in and sign-up functionality

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import React from "react";
+
+export const signIn = async () => {
+  try {
+  } catch (error) {
+    console.error("Unable to sign in: ", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+  } catch (error) {
+    console.error("Unable to sign in: ", error);
+  }
+};
