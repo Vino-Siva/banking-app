e6943badec9d9b7147e6cdce294dd53ad2bbfd2f
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-10-26 18:23:31 +0530

    Refactor appwrite.ts to handle missing environment variables

--- lib/server/appwrite.ts
+++ lib/server/appwrite.ts
@@ -1,13 +1,17 @@
-// src/lib/server/appwrite.js
 "use server";
 
 import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
 export async function createSessionClient() {
+  if (!process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    throw new Error("No Appwrite Endpoint");
+  if (!process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+    throw new Error("No Appwrite Project");
+
   const client = new Client()
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT);
 
   const session = cookies().get("appwrite-session");
   if (!session || !session.value) {
@@ -24,10 +28,16 @@ export async function createSessionClient() {
 }
 
 export async function createAdminClient() {
+  if (!process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    throw new Error("No Appwrite Endpoint");
+  if (!process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+    throw new Error("No Appwrite Project");
+  if (!process.env.NEXT_APPWRITE_KEY) throw new Error("Missing Appwrite Key");
+
   const client = new Client()
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-    .setKey(process.env.NEXT_APPWRITE_KEY!);
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+    .setKey(process.env.NEXT_APPWRITE_KEY);
 
   return {
     get account() {
