1bac582791d57a3791078b68587d03992b2c054f
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-10-27 18:15:46 +0530

    Refactor RightSidebar component to display dynamic userName

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -11,15 +11,12 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className="profile">
           <div className="profile-img">
             <span className="text-4xl font-bold text-blue-500">
-              {user.firstName.charAt(0)}
-              {user?.lastName?.charAt(0) || ""}
+              {user?.name?.charAt(0)}
             </span>
           </div>
           <div className="profile-details">
-            <h1 className="profile-name">
-              {user.firstName} {user.lastName}
-            </h1>
-            <p className="profile-email">{user.email}</p>
+            <h1 className="profile-name">{user?.name}</h1>
+            <p className="profile-email">{user?.email}</p>
           </div>
         </div>
       </section>
@@ -37,7 +34,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               <BankCard
                 key={banks[0].$id}
                 account={banks[0]}
-                userName={`${user.firstName} ${user.lastName}`}
+                userName={`${user?.name}`}
                 showBalance={false}
               />
             </div>
@@ -46,7 +43,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
                 <BankCard
                   key={banks[1].$id}
                   account={banks[1]}
-                  userName={`${user.firstName} ${user.lastName}`}
+                  userName={`${user?.name}`}
                   showBalance={false}
                 />
               </div>
