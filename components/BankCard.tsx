7215165dd7be26118a34673710a64dde434bf7e6
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-10-27 18:12:29 +0530

    Refactor BankCard to show dynamic userName

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -14,7 +14,7 @@ const BankCard = ({
         <div className="bank-card_content">
           <div className="">
             <h1 className="text-16 font-semibold text-white">
-              {account?.name?.toUpperCase() || userName.toUpperCase()}
+              {userName.toUpperCase()}
             </h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
